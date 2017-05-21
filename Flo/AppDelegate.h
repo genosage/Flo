1f3901834c8a1869986655afde20b5860cc16c2c
Josh Zhang <tianz2@student.unimelb.edu.au>
2017-05-21 16:55:17 +1000

    Core Graphics first commit

---
+++ Flo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flo
+//
+//  Created by 张添 on 21/5/17.
+//  Copyright © 2017 Tian Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flo
+//
+//  Created by 张添 on 21/5/17.
+//  Copyright © 2017 Tian Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
